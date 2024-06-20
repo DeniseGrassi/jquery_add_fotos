b884cffa57d2696e13e100bb33177c807d2a6995
Denise Grassi <denygrassi_@hotmail.com>
2024-06-20 12:56:53 -0300

    comentarios gpt

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 //Como add eventos com os elementos JQUERY !
 
 const header = document.querySelector('header button')
-$(document).ready(function() {    //p chamar o jquery usamos o $
+$(document).ready(function() {    //pratica comum! é usado para garantir que a FUNCTION so vai rodar dps que todo o DOCUMENT HTML estiver carregado
 
-//outra forma mais simplificada de fazer:
     $('header button').click(function() {
-        $('form').slideDown();  //quando clicar no botão NOVA IMAGEM+ o formulario vai descer
+        $('form').slideDown();  //quando clicar no botão NOVA IMAGEM+ o formulario vai descer com uma animação suave.
     }) 
 
     $('#botao-cancelar').click(function() {
-        $('form').slideUp();  //quando clicar no botão CANCELAR o formulario vai subir
+        $('form').slideUp();  //quando clicar no botão CANCELAR o formulario vai subir com uma animacao suave.
     }) 
 
-//segunda forma de add eventos
-    $('form').on('submit',function(e) {
+    $('form').on('submit',function(e) {  //qnd clicamos no botao ADD
         e.preventDefault();
-        const endImgNova = $('#end-img-nova').val();   //val = value. Estamos pegando as img novas subsmetidas.
-        const novoItem = $ ('<li style="display:none" >  </li>');              //as imgs novas submetidas tem que ficar nas tags LI.
-        
-        $ (`<img src=" ${endImgNova}" />`).appendTo(novoItem);   //essa funcao pega o end da img nova submetida e coloca na const novo item, ou seja, dentro das tags ti.
+        const endImgNova = $('#end-img-nova').val();   //val = value. Obtemos o valor do campo de entrada do URL e armazena na variavel const endImgNova.
+        const novoItem = $ ('<li style="display:none" >  </li>');   //criacao de um novo item na lista. As imgs novas submetidas ficam nas tags LI. 
+                                                                    //display:none para nao serem exibidas agora, so mais tarde com o fadeIn()
+        $ (`<img src=" ${endImgNova}" />`).appendTo(novoItem);   //cria uma nova IMG com o SRC da URL submetida! essa funcao pega o end da img nova submetida e coloca na const novo item, ou seja, dentro das tags ti.
 
-        $(` <div class="overlay-imagem-link">
+        $(` <div class="overlay-imagem-link">           
                 <a href="${endImgNova}" title="Ver imagem em tamanho real" target="_blank">
                 Ver imagem em tamanho real
                 </a>
-            </div> `).appendTo(novoItem);
+            </div> `).appendTo(novoItem);  // uma nova div é criada com as informacoes da nova foto add
 
-        $(novoItem).appendTo('ul');  //o novoItem aqui já tem todas as conf para entrar na tag UL                
-        $(novoItem).fadeIn(1000);
-        $('#end-img-nova').val('');  //vamos limpar o campo dps que foi add uma nova url. pra isso selecionamos o campo
+        $(novoItem).appendTo('ul');  //novo item é add na galeria! o novoItem aqui já tem todas as conf para entrar na tag UL                
+        $(novoItem).fadeIn(1000);  //a funcao fadeIn() faz com que o item apareca gradualmente ao longo de 1 segundo = 1000 milisegundos
+        $('#end-img-nova').val('');  //vamos limpar o campo dps que foi add uma nova url. pra isso selecionamos o campo pelo id.
     })    
 }) 
 
